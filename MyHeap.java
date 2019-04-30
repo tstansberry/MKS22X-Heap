bbcaee317bbdc87f9556b7e9121509d897de7fb8
tstansberry <tstansberry00@stuy.edu>
2019-04-30 09:47:41 -0400

    Created files

---
+++ MyHeap.java
@@ -0,0 +1,18 @@
+public class MyHeap {
+
+  private static void pushDown(int[]data,int size,int index) {
+
+  }
+
+  private static void pushUp(int[]data,int index) {
+
+  }
+
+  public static void heapify(int[]) {
+
+  }
+
+  public static void heapsort(int[]) {
+    
+  }
+}
